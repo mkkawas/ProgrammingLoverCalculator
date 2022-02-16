84413ce085d309cc42cfe29f19bd909fc0ff2f13
Mohamad Kawas <mohamad.kawas@lau.edu>
2022-02-16 14:03:55 +0200

    test

--- app/src/main/java/com/example/programminglovercalculator/MainActivity.java
+++ app/src/main/java/com/example/programminglovercalculator/MainActivity.java
@@ -39,6 +39,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 //for nothing
+
+
         
 
 
